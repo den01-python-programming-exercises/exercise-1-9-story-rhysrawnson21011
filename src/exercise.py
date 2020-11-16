c6df23b8e8b7b6f4d8e110880d6bc120dd77f448
rhysrawnson2101 <Rhysrawnson@gmail.com>
2020-11-16 13:45:21 +0000

    creating a story by re printing user input

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,17 @@
 def main():
     #write your code below this line
+  print("what is the main charaters name?")
+  Name = input()
+  print("what is thier job")
+  Job = input()
+  print ("here is the story:")
+  print ("once apon a time there was a person called " +  Name, "he is a " + Job) 
+  
+   
+
+
+
 
 if __name__ == '__main__':
+
     main()
